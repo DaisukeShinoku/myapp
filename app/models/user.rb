c7103871397851b39562009ba4d6dafd20845b79
daisuke.shinoku <daisuke.shinoku@appirits.com>
2020-11-25 14:57:06 +0900

    [Add]users_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,25 @@
 class User < ApplicationRecord
+  before_save   :downcase_email
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  has_secure_password
+
+  validates :department_id, numericality: {only_integer: true}
+  validates :name, presence: true, length: {maximum: 30}
+  validates :employee_id, numericality: {only_integer: true}
+  validates :email, presence: true, length: { maximum: 50 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  validates :is_valid, presence: true
+
+  has_many :requests
+
+  belongs_to :department
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
 end
--- db/migrate/20201124083906_create_departments.rb
+++ db/migrate/20201124083906_create_departments.rb
@@ -2,6 +2,7 @@ class CreateDepartments < ActiveRecord::Migration[6.0]
   def change
     create_table :departments do |t|
 
+      t.string :anagram, null: false
       t.string :name, null: false
       t.boolean :is_valid, default:true, null: false
       
