4cee2980559f5aaa5986c601396c419a1abcedbb
daisuke.shinoku <daisuke.shinoku@appirits.com>
2020-12-01 12:47:56 +0900

    [Add]admin_auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: [:new, :create]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   def new
     @user = User.new
@@ -7,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       redirect_to @user
     else
       render 'new'
@@ -34,10 +38,31 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_url
+  end
+  
+
   private
 
   def user_params
-    params.require(:user).permit(:department_id, :name, :employee_id, :email, :password, :password_confirmation, :avatar, :is_valid)
+    params.require(:user).permit(:department_id, :name, :employee_id, :email, :password, :password_confirmation, :avatar)
   end
 
+  def logged_in_user
+    unless logged_in?
+      store_location
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'homes#top'
-  resources :users, :except => [:new, :destroy]
+  resources :users, :except => [:new]
   get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
